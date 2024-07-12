0b27f52189294ca88b20f563269751e0ba58f0b9
ixn3rd3mxn <119990214+ixn3rd3mxn@users.noreply.github.com>
2024-07-12 23:56:43 +0700

    Delete discord.py

--- discord.py
+++
@@ -1,6 +0,0 @@
-from discordwebhook import Discord
-
-discord = Discord(url="https://discord.com/api/webhooks/1257947616377966733/td3usfNIUCpNTtJHGsqCc5_dOFHwwlOjJiuU49YQybKFYsnnnHtf7cS_4q_hqDUrUw58")
-discord.post(
-    embeds=[{"title": "โหลครับ โหลครับ", "โหลครับ โหลครับ": "Embed description"}],
-)
