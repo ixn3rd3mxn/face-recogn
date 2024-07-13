8d20a3a5b78ddcef7a59492f73e0d6bc94f76a58
ixn3rd3mxn <119990214+ixn3rd3mxn@users.noreply.github.com>
2024-07-13 16:17:53 +0700

    first_ver
    first_ver

---
+++ first ver/AddDatatoDatabase.py
@@ -0,0 +1,24 @@
+#this is AddDatatoDatabase.py
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL':"https://faceplus-1b3e4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "001":
+        {
+            "name": "Som Slash",
+            "total_attendance": 7,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
