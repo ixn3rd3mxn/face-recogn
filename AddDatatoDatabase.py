fd873fb1b0a2734ee8f80526fac52c7081fc125a
ixn3rd3mxn <119990214+ixn3rd3mxn@users.noreply.github.com>
2024-07-14 10:39:56 +0700

    x
    x

--- AddDatatoDatabase.py
+++ AddDatatoDatabase.py
@@ -9,26 +9,26 @@ firebase_admin.initialize_app(cred, {
     'databaseURL':"https://faceplus-1b3e4-default-rtdb.asia-southeast1.firebasedatabase.app/"
 })
 
-ref = db.reference('Students')
+ref = db.reference('Students') #create table or replace this name table in database
 
 data = {
     "001":
         {
             "name": "Som Slash",
-            "total_attendance": 7,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "total_attendance": 0,
+            "last_attendance_time": "0001-01-01 01:01:01"
         },
     "002":
         {
             "name": "Lee Tohde",
-            "total_attendance": 7,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "total_attendance": 0,
+            "last_attendance_time": "0001-01-01 01:01:01"
         },
     "003":
         {
             "name": "Wa Todenk",
-            "total_attendance": 7,
-            "last_attendance_time": "2022-12-11 00:54:34"
+            "total_attendance": 0,
+            "last_attendance_time": "0001-01-01 01:01:01"
         },
 }
 
